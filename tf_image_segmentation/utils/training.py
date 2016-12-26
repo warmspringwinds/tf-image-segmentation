5b3208c3be45ed1749b68df8f8beb59d44678ca6
Daniil Pakhomov <warmspringwinds@gmail.com>
2016-12-25 21:38:05 -0500

    the masking out value was made to be set by user to any value

--- tf_image_segmentation/utils/training.py
+++ tf_image_segmentation/utils/training.py
@@ -1,8 +1,5 @@
 import tensorflow as tf
 
-# Label value that is used to mark values
-# that should be eleminated from training
-MASK_OUT_LABEL = 255
 
 def get_labels_from_annotation(annotation_tensor, class_labels):
     """Returns tensor of size (width, height, num_classes) derived from annotation tensor.
@@ -16,22 +13,32 @@ def get_labels_from_annotation(annotation_tensor, class_labels):
     annotation_tensor : Tensor of size (width, height, 1)
         Tensor with class labels for each element
     class_labels : list of ints
-        List that contains the numbers that represent classes.
+        List that contains the numbers that represent classes. Last
+        value in the list should represent the number that was used
+        for masking out.
     Returns
     -------
     labels_2d_stacked : Tensor of size (width, height, num_classes).
         Tensor with labels for each pixel.
     """
     
+    # Last value in the classes list should show
+    # which number was used in the annotation to mask out
+    # the ambigious regions or regions that should not be
+    # used for training.
+    # TODO: probably replace class_labels list with some custom object
+    valid_entries_class_labels = class_labels[:-1]
+    
     # Stack the binary masks for each class
-    labels_2d = map(lambda x: tf.equal(annotation_tensor, x), class_labels)
+    labels_2d = map(lambda x: tf.equal(annotation_tensor, x),
+                    valid_entries_class_labels)
 
     # Perform the merging of all of the binary masks into one matrix
     labels_2d_stacked = tf.stack(labels_2d, axis=2)
     
     return labels_2d_stacked
 
-def get_valid_entries_indices_from_annotation(annotation_tensor):
+def get_valid_entries_indices_from_annotation(annotation_tensor, class_labels):
     """Returns tensor of size (num_valid_eintries, 2).
     Returns tensor that contains the indices of valid entries according
     to the annotation tensor. This can be used to later on extract only
@@ -40,17 +47,29 @@ def get_valid_entries_indices_from_annotation(annotation_tensor):
     ----------
     annotation_tensor : Tensor of size (width, height, 1)
         Tensor with class labels for each element
+    class_labels : list of ints
+        List that contains the numbers that represent classes. Last
+        value in the list should represent the number that was used
+        for masking out.
     Returns
     -------
     valid_labels_indices : Tensor of size (num_valid_eintries, 2).
         Tensor with indices of valid entries
     """
     
+    # Last value in the classes list should show
+    # which number was used in the annotation to mask out
+    # the ambigious regions or regions that should not be
+    # used for training.
+    # TODO: probably replace class_labels list with some custom object
+    mask_out_class_label = class_labels[-1]
+    
     # Get binary mask for the pixels that we want to
     # use for training. We do this because some pixels
     # are marked as ambigious and we don't want to use
     # them for trainig to avoid confusing the model
-    valid_labels_mask_2d = tf.not_equal(annotation_tensor, MASK_OUT_LABEL)
+    valid_labels_mask_2d = tf.not_equal(annotation_tensor,
+                                        mask_out_class_label)
     
     valid_labels_indices = tf.where(valid_labels_mask_2d)
     
